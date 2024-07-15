25fbfd84eeaf35e68990bff7243a4b335058e15a
Reaper <kmr.arnav3484@gmail.com>
2024-07-15 22:22:18 +0530

    Added basic foundational structure to main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+
+def display_menu():
+    print('1: Show Budget')
+    print('2: Quit')
+
+
+# Take Input
+def take_user_input():
+    pass
+
+running = True
+
+while running:
+    display_menu()
+    take_user_input()
