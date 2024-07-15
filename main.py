49d75377d086d2f2e6c07b15bcb931c4ce30c03c
Naman <namansingh302004@gmail.com>
2024-07-15 22:52:55 +0530

    Added list fro menu

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ import os
 def display_menu():
     print('1: Show Budget')
     print('2: Quit')
+    print("3: Show Budget")
+    print("4: Allocate Budget")
+    print("5: Add Expense")
+    print("6: Settings")
+    print("7: Show report")
+    print("8: Add Income Source")
+    print("Recurring Expense")
+    print("Filter")
+    print("User Profile and authentication")
+    print("Quit")
 
 
 # Take Input
