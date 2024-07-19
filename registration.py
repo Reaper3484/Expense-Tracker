0360b8010dfaedb9ef89a06259e1f4df60a6ad22
Naman <namansingh302004@gmail.com>
2024-07-19 18:56:20 +0530

    registration function complete

--- registration.py
+++ registration.py
@@ -1,5 +1,44 @@
+import json
+
+
 def register():
-    name = input("Enter your name : ")
-    username = input("Choose a username : ")
-    pwd=input("Type in a 6-digit Password : ")
+    with open('Data.json','r') as Data:
+        user=json.load(Data)
+    name=''
+    username=''
+    pwd=''   
+    VALID = True
+    CORRECT = True
+    while CORRECT:
+        name = input("Enter your name : ")
+        username = input("Choose a username : ")
+        pwd=input("Type in a 6-digit Password : ")
+    
+        VALID = True
+
+        for i in user['users']:
+            if i['username'] ==username:
+                VALID=False
+                CORRECT=True
+                print("That username is already taken. Please Try Again!!!")
+                retry = input("do you want to retry : Y/N : ")
+                if retry=='Y':
+                    break
+                elif retry=='N':
+                    print("THANK YOU !!!")
+                    break
+                else:
+                    print("Not a Valid Input")
+        if VALID:
+            break  
+    if VALID:
+        user['users'].append({
+            'name': name,
+            'username':username,
+            'pwd': pwd
+            })
+
+    with open('Data.json','w') as Data:
+        json.dump(user, Data, indent=4)
+
     
