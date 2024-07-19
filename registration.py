61fac55d9d0e3e0e3c9a80b75b596e8bade47c75
Naman <namansingh302004@gmail.com>
2024-07-19 21:23:40 +0530

    Registration func final

--- registration.py
+++ registration.py
@@ -1,78 +1,79 @@
 import json
 import os
 
-def is_empty(obj):
-    empty=False
-    if obj=='':
-        empty=True
-    return empty
-
-def pass_length(password):
-    if len(password)!=6:
-        return False
-    
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def is_empty(obj):
-    empty=False
-    if obj=='':
-        empty=True
-    return empty
+    return obj == ''
 
 def pass_length(password):
-    if len(password)==6:
-        return True
-    
+    return len(password) == 6
 
 def register():
-    with open('Data.json','r') as Data:
-        user=json.load(Data)
-    name=''
-    username=''
-    pwd=''   
+    
+    with open('Data.json', 'r') as Data:
+        user = json.load(Data)
+    
+    name = ''
+    username = ''
+    pwd = ''
     VALID = True
     CORRECT = True
+    
     while CORRECT:
-        name = input("Enter your name : ")
+        clear()  
+        name = input("Enter your name: ")
         if is_empty(name):
-            print("Please Enter a Name and then continue : ")
+            print("Please Enter a Name and then continue.")
+            input("Press Enter to continue...")  
             continue
-        username = input("Choose a username : ")
+        
+        clear()  
+        username = input("Choose a username: ")
         if is_empty(username):
-            print("Please Enter a Username and then continue : ")
+            print("Please Enter a Username and then continue.")
+            input("Press Enter to continue...")
             continue
-        pwd=input("Type in a 6-digit Password : ")
+        
+        clear()  
+        pwd = input("Type in a 6-digit Password: ")
         if is_empty(pwd):
-            print("Please Enter a Password and then continue : \n")
+            print("Please Enter a Password and then continue.")
+            input("Press Enter to continue...")
             continue
         elif not pass_length(pwd):
-            print("Password should be 6 characters Long")
+            print("Password should be 6 characters Long.")
+            input("Press Enter to continue...")
             continue
-        VALID = True
-
-        for i in user['users']:
-            if i['username'] ==username:
-                VALID=False
-                CORRECT=True
-                print("That username is already taken. Please Try Again!!!")
-                retry = input("do you want to retry : Y/N : ")
-                if retry.upper()=='Y':
-                    break
-                elif retry.upper()=='N':
-                    print("THANK YOU !!!")
-                    break
-                else:
-                    print("Not a Valid Input")
-        if VALID:
-            break  
         
+        VALID = True
+        for i in user['users']:
+            if i['username'] == username:
+                VALID = False
+                CORRECT = True
+                print("That username is already taken. Please Try Again!!!")
+                retry = input("Do you want to retry: Y/N: ")
+                if retry.upper() == 'Y':
+                    break
+                elif retry.upper() == 'N':
+                    print("THANK YOU!!!")
+                    return
+                else:
+                    print("Not a Valid Input.")
+                    input("Press Enter to continue...")
+        
+        if VALID:
+            break
+    
     if VALID:
         user['users'].append({
             'name': name,
-            'username':username,
+            'username': username,
             'pwd': pwd
-            })
-
-    with open('Data.json','w') as Data:
-        json.dump(user, Data, indent=4)
-
+        })
     
+    with open('Data.json', 'w') as Data:
+        json.dump(user, Data, indent=4)
+    
+    print("Registration Successful!")
