eec756399cb5bd43955a238a08626ef4038f34ba
Naman <namansingh302004@gmail.com>
2024-07-19 21:55:12 +0530

    Added new module for User profile

---
+++ user_profile.py
@@ -0,0 +1,15 @@
+class User:
+    def __init__(self,name,username,pwd):
+        self.name = name
+        self.username = username
+        self.pwd = pwd
+        
+        self.expenses=[]
+        self.budget=0
+        
+    def change_pass(self):
+        old = input("Enter Old Password : ")
+        if old==self.pwd:
+            new_pwd = input("Enter New Password : ")
+            self.pwd = new_pwd
+        
