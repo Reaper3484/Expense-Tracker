28513126eeadc569f8d52cfb86e7e899f65572d3
Naman <namansingh302004@gmail.com>
2024-07-21 21:55:01 +0530

    Added Details list in expenses.py

--- expenses.py
+++ expenses.py
@@ -7,9 +7,10 @@ def expenses(current_user,spend):
     for i in user["users"]:
         if current_user == i["username"]:
             now = datetime.now()
-            formatted_datetime = now.strftime("%Y-%m-%d %H:%M:%S")
-            new_expense={formatted_datetime:spend}
-            i["expenses"].append(new_expense)
+            date = now.strftime("%Y-%m-%d")
+            time = now.strftime("%H:%M:%S")
+            details = {"amount":spend,"date":date,"time":time}
+            i["details"].append(details)
     with open('user_profile.json', 'w') as Data:
         json.dump(user, Data, indent=4)
 
@@ -25,17 +26,20 @@ def allocate_budget(current_user,budget):
         json.dump(user, Data, indent=4)
 
 
-def money_spent(current_user,spend):
-    expenses(current_user,spend)
+def money_spent(current_user, spend):
+    
+    expenses(current_user, spend)
+    
+    
     with open('user_profile.json', 'r') as Data:
         user = json.load(Data)
+    
+    
     for i in user["users"]:
-        latest_expense_sum=0
         if current_user == i["username"]:
-            for j in i["expenses"]:
-                latest_expense_sum+=sum(j.values())
-            i["money_spent"]=latest_expense_sum
+            
+            latest_expense_sum = sum(item["amount"] for item in i["details"])
+            i["money_spent"] = latest_expense_sum
+    
     with open('user_profile.json', 'w') as Data:
         json.dump(user, Data, indent=4)
-    
-    
